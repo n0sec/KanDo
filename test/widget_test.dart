cf9222baee27926d5451bd2893cba77f088513f4
Nick Conklin <nconkl1@outlook.com>
2022-02-12 14:21:00 -0500

    Added Drawer to Home Page
    - This will probably change later on
    - I don't really like the gap from the top to the DrawerHeader
    - Can change this later

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kan_do/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const KanDo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
