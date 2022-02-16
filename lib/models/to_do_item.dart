c69949d36c75ce151a8d0575829c778879863cda
Nick Conklin <nconkl1@outlook.com>
2022-02-16 18:53:38 -0500

    Added ToDoItem Model
    - Created the model to use with a Task/To Do Item

---
+++ lib/models/to_do_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+enum Priority {
+  low,
+  medium,
+  high,
+}
+
+class ToDoItem {
+  final String id;
+  final String? label;
+  final Color? labelColor;
+  final Enum priority;
+  final String? description;
+  final String taskName;
+  final DateTime? dueDate;
+  bool isCompleted;
+
+  ToDoItem({
+    required this.id,
+    this.label,
+    this.labelColor,
+    this.priority = Priority.low,
+    this.description,
+    required this.taskName,
+    this.dueDate,
+    this.isCompleted = false,
+  });
+}
